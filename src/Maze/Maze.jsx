33d2d7996aa55799cead6a51c836857bb0068d8e
tomsl123 <tslezak4@gmail.com>
2024-07-20 13:19:03 +0200

    basic maze layout

--- src/Maze/Maze.jsx
+++ src/Maze/Maze.jsx
@@ -1,6 +1,7 @@
 import './Maze.css'
 import { Transition } from 'react-transition-group';
-import {useRef, useState} from "react";
+import {createElement, useRef, useState} from "react";
+import labyrinthos from "labyrinthos";
 
 const duration = 300;
 
@@ -16,11 +17,13 @@ const transitionStyles = {
     exited:  { opacity: 0 },
 };
 
-function Maze(props) {
+const maze = prepareMaze(6,7);
+const endAndStartPoint = findFurthestPoints(maze);
 
-    console.log(prepareMazeArray())
-    const [inProp, setInProp] = useState(props.initInProp);
+function Maze(props) {
     const nodeRef = useRef(null);
+    const [inProp, setInProp] = useState(props.initInProp);
+    const [characterPos, setCharacterPos] = useState(endAndStartPoint[1]);
 
     setTimeout(() => {
         setInProp(true);
@@ -35,24 +38,103 @@ function Maze(props) {
                     ...defaultStyle,
                     ...transitionStyles[state]
                 }}>
-
+                    {renderMaze(maze)}
                 </div>
             )}
         </Transition>
     )
 }
 
-function prepareMazeArray() {
-    const mazeArray = [];
-    for (let i = 0; i < 7; i++) {
-        mazeArray.push([]);
-        for (let j = 0; j < 6; j++) {
-            mazeArray[i].push(1);
+function prepareMaze(width, height) {
+    let map = new labyrinthos.TileMap({
+        width: width,
+        height: height
+    });
+    map.fill(1);
+    labyrinthos.mazes.BinaryTree(map, {});
+
+    let mazeArray = [];
+    for (let i = 0; i < map.data.length; i++) {
+        if(Number.isInteger(i / width)) {
+            mazeArray.push([]);
+        }
+        mazeArray[Math.floor(i / width)].push(map.data[i]);
+    }
+
+    return mazeArray;
+}
+
+function findFurthestPoints(maze) {
+    const rows = maze.length;
+    const cols = maze[0].length;
+    const visited = new Set();
+    let maxDistance = 0;
+    let startPoint = null;
+    let endPoint = null;
+
+    // Helper function to perform BFS
+    function bfs(startX, startY) {
+        const queue = [[startX, startY]];
+        visited.add(`${startX},${startY}`);
+        let distance = 0;
+
+        while (queue.length > 0) {
+            const size = queue.length;
+            for (let i = 0; i < size; i++) {
+                const [x, y] = queue.shift();
+                if (distance > maxDistance) {
+                    maxDistance = distance;
+                    startPoint = [startX, startY];
+                    endPoint = [x, y];
+                }
+
+                for (const [dx, dy] of [[0, 1], [0, -1], [1, 0], [-1, 0]]) {
+                    const newX = x + dx;
+                    const newY = y + dy;
+                    if (
+                        newX >= 0 &&
+                        newX < rows &&
+                        newY >= 0 &&
+                        newY < cols &&
+                        maze[newX][newY] === 0 &&
+                        !visited.has(`${newX},${newY}`)
+                    ) {
+                        queue.push([newX, newY]);
+                        visited.add(`${newX},${newY}`);
+                    }
+                }
+            }
+            distance++;
         }
     }
 
+    // Perform BFS from each path point
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            if (maze[i][j] === 0 && !visited.has(`${i},${j}`)) {
+                bfs(i, j);
+            }
+        }
+    }
 
-    return mazeArray;
+    return [startPoint, endPoint];
+}
+
+function renderMaze(maze) {
+    const mazeSquares = [];
+    const rowBreakStyle = {gridTemplateColumns: `repeat(${maze[0].length}, 1fr)`};
+
+    for (let row = 0; row < maze.length; row++) {
+        for (let column = 0; column < maze[row].length; column++) {
+            const id = `square_${row}_${column}`;
+            const squareColorStyle = maze[row][column] === 1 ? 'wall' : 'path'
+
+            mazeSquares.push(createElement('div', {id: id, key: id, className: `maze-square ${squareColorStyle}`}))
+        }
+    }
+
+    return createElement('div', {className: 'maze', style: rowBreakStyle}, mazeSquares);
+
 }
 
 export default Maze
